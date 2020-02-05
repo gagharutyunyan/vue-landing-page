7203c23aa4ced3bb1fe36f662c72ffb9146cd5ae
gagharutyunyan <gagharut@gmail.com>
2020-02-06 01:46:15 +0300

    add new block assistant

--- src/components/Blocks/Assistant/Assistant.vue
+++ src/components/Blocks/Assistant/Assistant.vue
@@ -1,15 +1,22 @@
 <template>
   <section class="section__assistant">
-    <div class="section__assistant">
-      <h1 class="assistant__title"><span></span></h1>
+    <div class="assistant">
+      <h1 class="assistant__title">
+        Персональный помощник<br />
+        <span>Бесплатно для владельцев <b>CARD PLUS</b></span>
+      </h1>
       <div class="assistant__content">
-        <div class="assistant__content__img"></div>
-        <div class="assistant__content__functions">
-          <ul class="assistant__functions__list">
-            <li class="list__item"></li>
-            <li class="list__item"></li>
-            <li class="list__item"></li>
-            <li class="list__item"></li>
+        <img
+          class="assistant__content__img"
+          src="https://i.postimg.cc/CKrPby1N/img-min.png"
+          alt="Персональный помощник"
+        />
+        <div class="assistant__content__function">
+          <ul class="assistant__function__list">
+            <li class="list__item" v-for="(item, i) in function__list" :key="i">
+              {{ item.description }}
+              <span class="icon"></span>
+            </li>
           </ul>
         </div>
       </div>
@@ -23,6 +30,30 @@
 
 <script>
 export default {
-  name: "Assistant"
+  name: "Assistant",
+  data() {
+    return {
+      function__list: [
+        {
+          description: "Организует встречи, найдет гидов и переводчиков",
+          icon: "vector-1"
+        },
+        {
+          description:
+            "Спланирует поездку по самым интересным местам вашего отдыха",
+          icon: "vector-1"
+        },
+        {
+          description:
+            "Достанет билеты на мероприятия, даже если их уже нет в продаже",
+          icon: "vector-1"
+        },
+        {
+          description: "Закажет столик в ресторане, клубе или кафе",
+          icon: "vector-1"
+        }
+      ]
+    };
+  }
 };
 </script>
