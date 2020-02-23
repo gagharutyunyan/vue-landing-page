4708f64b0b31e2c54a7bca33dc17a3355e43b63d
gagharutyunyan <gagharut@gmail.com>
2020-02-23 11:49:28 +0300

    the latest deploy

---
+++ precache-manifest.9e5aeb41e68f571fe0002419c7873aa5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c896c5cb568b1fc5b66",
+    "url": "/vue-landing-page/css/app.8563c36d.css"
+  },
+  {
+    "revision": "931c0593c98ad1c4bdcb",
+    "url": "/vue-landing-page/css/chunk-50975de4.865c5056.css"
+  },
+  {
+    "revision": "bfaff1db4a84062c67da9c379d32bea8",
+    "url": "/vue-landing-page/img/bg.bfaff1db.png"
+  },
+  {
+    "revision": "daa81889c79404b8c04b64b1c6e72a92",
+    "url": "/vue-landing-page/img/credit-card.daa81889.png"
+  },
+  {
+    "revision": "0beee8357e836fbf03b2a82138fb2422",
+    "url": "/vue-landing-page/img/logo.0beee835.svg"
+  },
+  {
+    "revision": "45fb8e98dbe457359d5abcc240316f02",
+    "url": "/vue-landing-page/index.html"
+  },
+  {
+    "revision": "0c896c5cb568b1fc5b66",
+    "url": "/vue-landing-page/js/app.e252989f.js"
+  },
+  {
+    "revision": "931c0593c98ad1c4bdcb",
+    "url": "/vue-landing-page/js/chunk-50975de4.e8889222.js"
+  },
+  {
+    "revision": "20f52db0d6afd3491e14",
+    "url": "/vue-landing-page/js/chunk-vendors.901db639.js"
+  },
+  {
+    "revision": "3e6a5e9c99ef6e8c29a97292d544a90b",
+    "url": "/vue-landing-page/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-landing-page/robots.txt"
+  }
+]);
